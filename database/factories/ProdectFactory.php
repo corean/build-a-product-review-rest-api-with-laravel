796a11661e2cafd3d4c930218a2e2afe9a57e0b4
corean <corean@corean.biz>
2020-08-17 15:50:04 +0900

    - Model

---
+++ database/factories/ProdectFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Prodect;
+use Faker\Generator as Faker;
+
+$factory->define(Prodect::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});

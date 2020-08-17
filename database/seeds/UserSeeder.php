b3b6330e47476380832980223b1033bc7d570003
corean <corean@corean.biz>
2020-08-17 17:30:28 +0900

    - JWT installed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,6 +11,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        \App\User::create([
+            'name' => 'corean',
+            'email' => 'corean@corean.biz',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
         factory(App\User::class, 50)->create();
     }
 }
